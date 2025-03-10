ddf216adee3ef18f6e86e838101de7cef52d2f0d
Carson Kohlbrenner <82614880+cKohl10@users.noreply.github.com>
2025-03-10 10:42:58 -0600

    bruh

--- proj1main.m
+++ proj1main.m
@@ -1,2 +1,29 @@
 % ASEN 5114 Project 1
-% Authors: Carosn Kohlbenner, Thomas Dunnington, and Owen Craig
+% Authors: Carosn Kohlbenner, Thomas Dunnington, and Owen Craig4
+clc;
+clear;
+close all;
+
+% Read the data from the file
+data = table2array(readtable('data\Spacecraft_spin_module_frequency_response_data.xlsx'));
+freq = data(:,1);
+amp = data(:,2);
+phase = data(:,3);
+
+% Plot the bode plot
+figure;
+semilogx(freq, db(amp));
+title('Bode Plot of Spacecraft Spin Module Frequency Response');
+xlabel('Frequency (Hz)');
+ylabel('Amplitude (dB)');
+grid on;
+
+% Plot the phase plot
+figure;
+semilogx(freq, phase);
+title('Phase Plot of Spacecraft Spin Module Frequency Response');
+xlabel('Frequency (Hz)');
+ylabel('Phase (deg)');
+grid on;
+
+
