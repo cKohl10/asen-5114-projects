f5824d527a407688f1d5694275bb55f641c06163
Owen Craig <owcr8621@colorado.edu>
2025-03-14 10:13:13 -0600

    Owen

--- owen.m
+++ owen.m
@@ -1,5 +1,5 @@
 % ASEN 5114 Project 1
-% Authors: Carosn Kohlbenner, Thomas Dunnington, and Owen Craig
+% Authors: Carosn Kohlbenner, Thomas Dunnington, and Owen Craig4
 clc;
 clear;
 close all;
@@ -13,16 +13,21 @@ amp_db_P0 = 20*log10(amp);
 phase_P = data(:,3) - pi/2;
 phase_P0 = data(:,3);
 
-%% Estimate the transfer function
-% Second order fit 
-DC_gain = 0.008;
-w0 = 0.4;
+% Transfer function with anti resonance, resonance pair
 s = tf('s');
-P = (DC_gain)/(s^2*(s/w0+1));
-[mag, phase_first, wout] = bode(P, freq);
+DC_gain = 0.041;
+omega_ar1 = 0.73;   % anti-resonance frequency
+omega_r1 = 1.32;    % resonance frequency
+omega_ar2 = 4.9;
+omega_r2 = 6.1;
+zeta_z = 0.015;
+zeta_p = 0.03;
+zeta_z2 = 0.03;
+zeta_p2 = 0.034;
+G = DC_gain * (1/s^2) * (s^2 + 2*zeta_z*omega_ar1*s + omega_ar1^2) / (s^2 + 2*zeta_p*omega_r1*s + omega_r1^2) * (s^2 + 2*zeta_z2*omega_ar2*s + omega_ar2^2) / (s^2 + 2*zeta_p2*omega_r2*s + omega_r2^2);
+[mag, phase, wout] = bode(G, freq);
 mag = squeeze(mag);
-
-% Estimate Transfer Function with Resonace Points
+phase = squeeze(phase);
 
 
 % Plot the bode plot
@@ -34,14 +39,20 @@ title('Bode Plot');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude (dB)');
 xlim([freq(1), freq(end)]);
+legend('P', 'P(s)');
 grid on;
+saveas(gcf, 'figs/bode_plot.png');
 
-% % Plot the phase plot
-% figure;
-% semilogx(freq, rad2deg(phase_P));
-% title('Phase Plot of Spacecraft Spin Module Frequency Response');
-% xlabel('Frequency (Hz)');
-% ylabel('Phase (deg)');
-% grid on;
-
+% Plot the phase plot
+figure;
+semilogx(freq, rad2deg(phase_P));
+hold on;
+semilogx(freq, (phase));
+title('Phase Plot');
+xlabel('Frequency (Hz)');
+ylabel('Phase (deg)');
+xlim([freq(1), freq(end)]);
+grid on;
+legend('P', 'TF Fit');
+saveas(gcf, 'figs/phase_plot.png');
 
